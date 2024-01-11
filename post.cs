5bb5c11233271d036fd017d5d76cc917ee464774
Genbodmas <esoolabode@gmail.com>
2024-01-11 11:18:01 +0100

    Blog Console App

---
+++ post.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Blog_ConsoleApp
+{
+    class post
+    {
+        public int BlogID { get; set; }
+        public int ID { get; set; }
+        public string Date { get; set; }
+        public string Title { get; set; }
+        public string Content { get; set; }
+    }
+}
